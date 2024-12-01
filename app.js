ec3d6e81a1a33c3965edb7644658e52e74cd8216
Talha Abdurehman <143262179+Talha-Abdurehman@users.noreply.github.com>
2024-12-02 02:38:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
